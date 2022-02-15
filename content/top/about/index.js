282c18cc25d164b792bff85cccc3982d729848f1
Ezra Citron <ezracitron@gmail.com>
2022-02-15 21:14:47 +0000

    updates

--- content/top/about/index.js
+++ content/top/about/index.js
@@ -1,25 +1,17 @@
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
-  
-  // Apply to all slideshows that you define with the markup wrote
-  slideshows.forEach(initSlideShow);
+var slideIndex = 1;
+showDivs(slideIndex);
 
-  function initSlideShow(slideshow) {
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
-    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
-
-    var index = 0, time = 5000;
-    slides[index].classList.add('active');  
-    
-    setInterval( () => {
-      slides[index].classList.remove('active');
-      
-      //Go over each slide incrementing the index
-      index++;
-      
-      // If you go over all slides, restart the index to show the first slide and start again
-      if (index === slides.length) index = 0; 
-      
-      slides[index].classList.add('active');
-
-    }, time);
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  x[slideIndex-1].style.display = "block";
+}
